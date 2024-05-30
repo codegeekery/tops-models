45edc53765d8d149ea20107db14932596b57f5ea
codegeekery <85375414+codegeekery@users.noreply.github.com>
2024-05-30 15:20:51 +0100

    new endpoints

--- utils/endpoint.js
+++ utils/endpoint.js
@@ -1,5 +1,5 @@
-const BASE_PATH = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/Blog/' : 'https://codegeekery.com/Blog/'
-const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://www.codegeekery.com/api'
-const ORIGIN_PATH = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://www.codegeekery.com'
+const BASE_PATH = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/Blog/' : 'https://tops-models.vercel.app/Blog/'
+const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://tops-models.vercel.app/api'
+const ORIGIN_PATH = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://tops-models.vercel.app'
 
 export { BASE_PATH, API_URL, ORIGIN_PATH}
